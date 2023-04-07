06b25d4958f1f269dde3618e392a80360e864f27
Ryan <barkmanr@sheridancollege.ca>
2023-04-06 22:10:58 -0400

    Changed connecting wall to better format

--- Connecting wall/Views/ConnectingWall.xaml.cs
+++ Connecting wall/Views/ConnectingWall.xaml.cs
@@ -6,4 +6,34 @@ public partial class ConnectingWall : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void WhenRevealedClicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void WhenAnswerClicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void WhenUpClicked1(object sender, EventArgs e)
+    {
+
+    }
+
+    private void WhenDownClicked1(object sender, EventArgs e)
+    {
+
+    }
+
+    private void WhenUpClicked2(object sender, EventArgs e)
+    {
+
+    }
+
+    private void WhenDownClicked2(object sender, EventArgs e)
+    {
+
+    }
 }

bb3fa4a246e5c6fc79b029b7928ec65e44fd3858
Gerard <debelena@shernet.sheridancollege.ca>
2023-04-14 11:43:38 -0400

    implementing round two board logic.

--- Connecting wall/Views/RoundTwoBoard.xaml.cs
+++ Connecting wall/Views/RoundTwoBoard.xaml.cs
@@ -1,9 +1,44 @@
 namespace Connecting_wall.Views;
 
-public partial class NewPage1 : ContentPage
+public partial class Round2Board : ContentPage
 {
-	public NewPage1()
+	public Round2Board()
 	{
 		InitializeComponent();
 	}
+
+    private void WhenRevealedClicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void WhenAnswerClicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void WhenUpClicked1(object sender, EventArgs e)
+    {
+
+    }
+
+    private void WhenDownClicked1(object sender, EventArgs e)
+    {
+
+    }
+
+    private void WhenUpClicked2(object sender, EventArgs e)
+    {
+
+    }
+
+    private void WhenDownClicked2(object sender, EventArgs e)
+    {
+
+    }
+
+    private void BackClicked(object sender, EventArgs e)
+    {
+
+    }
 }
